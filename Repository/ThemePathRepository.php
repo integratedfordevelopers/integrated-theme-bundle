13aa454998a448bd846f95da2744b48242e5322c
Christiaan Goslinga <christiaangoslinga@gmail.com>
2017-06-28 10:18:11 +0200

    Added author doc.

--- Repository/ThemePathRepository.php
+++ Repository/ThemePathRepository.php
@@ -1,17 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Christiaan Goslinga
- * Date: 5-4-2017
- * Time: 14:51
- */
+
+/*
+* This file is part of the Integrated package.
+*
+* (c) e-Active B.V. <integrated@e-active.nl>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
 
 namespace Integrated\Bundle\ThemeBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
 
+/**
+ * @author Christiaan Goslinga
+ */
 class ThemePathRepository extends EntityRepository
 {
+    /**
+     * {@inheritdoc}
+     */
     public function findOneByThemePath($path, $content)
     {
         return $this->createQueryBuilder('themepath')
